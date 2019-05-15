4f85fabf0de2c5b3bf557a80d1c8f2d95165c778
liam humphreys <liam.humphreys1989@gmail.com>
2019-05-15 07:34:23 +0100

    more acceptance tests

--- implementation/web/server_acceptance_test.go
+++ implementation/web/server_acceptance_test.go
@@ -21,6 +21,7 @@ func validPayment() *payments.Payment {
 		OrganisationID: uuid.New().String(),
 	}
 }
+
 func Test_HTTP_Restful_Correctness(t *testing.T) {
 	address := fmt.Sprintf("%s/payments", testhelpers.APIAddress(t))
 
@@ -28,7 +29,7 @@ func Test_HTTP_Restful_Correctness(t *testing.T) {
 
 		deleteAll(t, address)
 
-		t.Run("Get Payments Returns Empty List", func(t *testing.T) {
+		t.Run("get payments returns empty list", func(t *testing.T) {
 			allPayments := []payments.Payment{}
 			resp, err := resty.R().SetResult(&allPayments).Get(address)
 			assert.Nil(t, err)
@@ -36,13 +37,13 @@ func Test_HTTP_Restful_Correctness(t *testing.T) {
 			assert.Empty(t, allPayments)
 		})
 
-		t.Run("Delete Payments Returns Success", func(t *testing.T) {
+		t.Run("delete payments returns success", func(t *testing.T) {
 			resp, err := resty.R().Delete(address)
 			assert.Nil(t, err)
 			assert.Equal(t, 200, resp.StatusCode())
 		})
 
-		t.Run("Put Payments Returns Success", func(t *testing.T) {
+		t.Run("put payments returns success", func(t *testing.T) {
 			allPayments := []payments.Payment{
 				*validPayment(),
 				*validPayment(),
@@ -56,10 +57,72 @@ func Test_HTTP_Restful_Correctness(t *testing.T) {
 			assert.Equal(t, allPayments, allNewPayments)
 		})
 	})
+
+	t.Run("When payments exist", func(t *testing.T) {
+		initialPayments := []payments.Payment{
+			*validPayment(),
+			*validPayment(),
+			*validPayment(),
+			*validPayment(),
+		}
+
+		deleteAll(t, address)
+		createAll(t, address, initialPayments)
+
+		t.Run("get payments returns all payments", func(t *testing.T) {
+			allPayments := []payments.Payment{}
+			resp, err := resty.R().SetResult(&allPayments).Get(address)
+			assert.Nil(t, err)
+			assert.Equal(t, 200, resp.StatusCode())
+			assert.Equal(t, initialPayments, allPayments)
+		})
+
+		t.Run("put same payments returns success", func(t *testing.T) {
+			allNewPayments := []payments.Payment{}
+			resp, err := resty.R().SetResult(&allNewPayments).SetBody(initialPayments).Put(address)
+			assert.Nil(t, err)
+			assert.Equal(t, 200, resp.StatusCode())
+			assert.Equal(t, initialPayments, allNewPayments)
+		})
+
+		t.Run("put new payments returns success", func(t *testing.T) {
+			newPayments := []payments.Payment{
+				*validPayment(),
+				*validPayment(),
+				*validPayment(),
+				*validPayment(),
+			}
+			allNewPayments := []payments.Payment{}
+			resp, err := resty.R().SetResult(&allNewPayments).SetBody(newPayments).Put(address)
+			assert.Nil(t, err)
+			assert.NotEqual(t, newPayments, initialPayments)
+			assert.Equal(t, 200, resp.StatusCode())
+			assert.Equal(t, newPayments, allNewPayments)
+		})
+
+		t.Run("delete payments returns success", func(t *testing.T) {
+			resp, err := resty.R().Delete(address)
+			assert.Nil(t, err)
+			assert.Equal(t, 200, resp.StatusCode())
+			allPayments := []payments.Payment{}
+			resp, err = resty.R().SetResult(&allPayments).Get(address)
+			assert.Nil(t, err)
+			assert.Equal(t, 200, resp.StatusCode())
+			assert.Empty(t, allPayments)
+		})
+	})
 }
 
 func deleteAll(t *testing.T, address string) {
+	t.Helper()
 	resp, err := resty.R().Delete(address)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, resp.StatusCode())
+}
+
+func createAll(t *testing.T, address string, allPayments []payments.Payment) {
+	t.Helper()
+	resp, err := resty.R().SetBody(allPayments).Put(address)
 	assert.Nil(t, err)
 	assert.Equal(t, 200, resp.StatusCode())
 }
